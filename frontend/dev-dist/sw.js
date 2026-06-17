8629da5e47c3a330a5e0ff3f12d2b9f62cda217a
jeremy <jeremycarhvalos@gmail.com>
2026-06-17 16:54:14 +0700

    fix: penyempurnaan fitur konfirmasi pembayaran dan tolak/batal order

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-afac4cd2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5g8n8148834"
+    "revision": "0.upgrsdm58bk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
