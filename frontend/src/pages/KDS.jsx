d4b0f53d8cf4dc0aa2018643170d6b5d4a019761
jeremy <jeremycarhvalos@gmail.com>
2026-06-24 18:37:11 +0700

    fix: KDS jsx syntax error missing parenthesis

--- frontend/src/pages/KDS.jsx
+++ frontend/src/pages/KDS.jsx
@@ -383,6 +383,7 @@ export default function KDS() {
                     </div>
                   ))}
                 </div>
+                )}
 
                 {/* Card Footer */}
                 {userCanEdit('kds') && (
