1a6cde761224ed643c528dcb218abebbadcb8289
jeremy <jeremycarhvalos@gmail.com>
2026-06-24 16:43:18 +0700

    Fix image url for bukti pembayaran

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-afac4cd2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.d0rb08n54m8"
+    "revision": "0.0gf9pibcrls"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
