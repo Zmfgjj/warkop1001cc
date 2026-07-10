fce82163f3f982412fe07265b5889f80b18c94ad
jeremy <jeremycarhvalos@gmail.com>
2026-07-10 16:37:51 +0700

    Remove PWA plugin entirely from project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,56 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpeg}'],
-        runtimeCaching: [
-          {
-            urlPattern: /\/uploads\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'upload-images-cache',
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
-              },
-              cacheableResponse: {
-                statuses: [0, 200]
-              }
-            }
-          }
-        ]
-      },
-      injectRegister: 'auto',
-      manifest: {
-        name: 'Warkop POS',
-        short_name: 'POS',
-        description: 'Warkop 1001 CC POS System',
-        theme_color: '#E8D5B7',
-        icons: [
-          {
-            src: 'logo-192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'any maskable'
-          },
-          {
-            src: 'logo-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      }
-    })
+    react()
   ],
   server: {
     host: true,
